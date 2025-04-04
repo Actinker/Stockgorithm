32b3b50d8f7af7778df27539554218fcb30bb092
Actinker <147242303+Actinker@users.noreply.github.com>
2025-04-04 20:19:45 +0530

    adding more from begining

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["three"], // Ensure Three.js is optimized
+  },
+  build: {
+    rollupOptions: {
+      external: ["three"], // Add this if Three.js is not bundling properly
+    },
+  },
+});
